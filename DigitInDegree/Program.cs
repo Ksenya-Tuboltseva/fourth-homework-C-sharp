348799f4536574f3c8b29169dc707b5bb655098b
Ksenya <ksyuhacr7@gmail.com>
2022-12-12 19:31:16 +0300

    initial commit

---
+++ DigitInDegree/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите первое число");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+if (B >= 1)
+{
+    for (int i = 1; i <= B; i++)
+    {
+        result *= A;
+    }
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Второе число - не натуральное");
+};
---
+++ SumOfDivisor/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int sizeOfNumber = 0; //Количество цифр числа
+int CopyOfNumber = number; //копия изначального числа number
+while(CopyOfNumber > 0)
+{
+    CopyOfNumber = CopyOfNumber / 10;
+    sizeOfNumber++;
+}
+
+int CopyOfNumber2 = number; //копия изначального числа number
+int [] arrayWithNumber = new int [sizeOfNumber]; //массив с длинной введенного числа
+for(int i = 0; i < sizeOfNumber; i++)
+{
+    arrayWithNumber[i] = CopyOfNumber2 % 10;
+    CopyOfNumber2 = CopyOfNumber2 / 10;
+}
+
+int sum = 0;  //сумма делителей
+for(int i = 0; i < sizeOfNumber; i++)
+{
+    if (arrayWithNumber[i] > 0)
+    {
+        if (number % arrayWithNumber[i] == 0)
+        {
+            sum = sum + arrayWithNumber[i];
+        };
+    };
+};
+Console.WriteLine(sum);
